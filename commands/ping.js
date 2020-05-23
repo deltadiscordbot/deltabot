ed31e792b9145213698b346d8e797bb3952f34e7
Jimmy <jimmyshadow0@gmail.com>
2020-05-23 14:55:05 -0500

    updated commands

--- commands/ping.js
+++ commands/ping.js
@@ -4,15 +4,11 @@ module.exports = {
     name: 'ping',
     description: 'Ping!',
     cooldown: 3,
-    execute(message, args) {
+    needsclient: true,
+    async execute(message, args,client) {
 
-       message.channel.send('Pong! in `' + `${Date.now() - message.createdTimestamp}` + 'ms`');
-        //message.channel.send("My Ping: " + Math.round(client.ping) + ' ms');
-       /* message.channel.send('${Math.round(client.ping)}');
-        async function ping(message) {
-        var resMsg = await message.channel.send('Pinging... <a:loading:597979197780000774>');
-        resMsg.edit('Ping: `' + Math.round((resMsg.createdTimestamp - message.createdTimestamp) - ${Math.round(client.ping)}) + "`");
-    }
-    ping(message);*/
-    },
+       //message.channel.send('Pong! in `' + `${Date.now() - message.createdTimestamp}` + 'ms`');
+       const m = await message.channel.send("Ping?");
+       m.edit(`Pong! Latency is \`${m.createdTimestamp - message.createdTimestamp}ms\`. API Latency is \`${client.ws.ping}ms\``);
+       },
 };
--- commands/uptime.js
+++ commands/uptime.js
@@ -4,15 +4,21 @@ module.exports = {
     name: 'uptime',
     description: `Uptime of ${package.name}.`,
     cooldown: 10,
+    aliases: ['up'],
     needsclient: true,
     execute(message, args, client) {
         let totalSeconds = (client.uptime / 1000);
         let days = Math.floor(totalSeconds / 86400);
+        totalSeconds %= 86400;
         let hours = Math.floor(totalSeconds / 3600);
         totalSeconds %= 3600;
         let minutes = Math.floor(totalSeconds / 60);
         let seconds = Math.floor(totalSeconds % 60);
-        let uptime = `${days}:${hours}:${minutes}:${seconds}`;
+        const formattedHours = ("0" + hours).slice(-2);
+        const formattedMinutes = ("0" + minutes).slice(-2);
+        const formattedSeconds = ("0" + seconds).slice(-2);
+
+        const uptime = `${days}:${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
         message.reply(uptime)
     },
 };
