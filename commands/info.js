ff7ec372717c02f24460d3b400478b43cba7cfeb
Jimmy <jimmyshadow0@gmail.com>
2020-05-23 13:27:07 -0500

    updated info command and version number

--- commands/info.js
+++ commands/info.js
@@ -11,14 +11,16 @@ module.exports = {
     const exampleEmbed = new Discord.MessageEmbed()
 	.setColor('#32CD32')
     .setTitle(package.name)
+    .setURL("https://github.com/deltadiscordbot/deltabot")
     .setThumbnail(client.user.avatarURL)
 	.setDescription(package.description)
+    .addField('Servers:', client.guilds.cache.size,true)
+    .addField('Users:', client.users.cache.size,true)
     .addField('Version:', package.version)
-    .setURL("https://github.com/deltadiscordbot/deltabot")
-	.addField('Library:','discord.js')
-	.addField('Creator:', package.author)
+	.addField('Library:','discord.js',true)
+	.addField('Creator:', package.author,true)
 	.setTimestamp()
-	.setFooter('version: ' + package.version);
+	.setFooter('Requested by: ' + message.author.tag);
 
     message.channel.send(exampleEmbed);
     },
