3e9ac646577be67454cc37f6bafa529e3d89a282
Terrain2 <37938646+Terrain2@users.noreply.github.com>
2020-06-08 19:56:57 +0200

    Update whois.js
    fix a bug (``!whois <any string that isn't a mention or "me">`` causing an error) and clean up the code a little

--- commands/whois.js
+++ commands/whois.js
@@ -1,15 +1,6 @@
 const { mongodbase, currentdb } = require('../config.json');
 const package = require('../package.json');
 const Discord = require('discord.js');
-var userCreated;
-var userRoles;
-var username;
-var userAvatar;
-var userJoinedServer;
-var userJoinPosition;
-var userID;
-var userObject;
-var memberObject;
 module.exports = {
   name: 'whois',
   description: `Shows information about a user`,
@@ -18,39 +9,19 @@ module.exports = {
   guildOnly: true,
   aliases: ['lookup'],
   execute(message, args) {
-    if (args.length) {
-      if (args[0] == "me"){
-        userObject = message.author;
-      } else{
-        userObject = message.mentions.users.first();
-      }
-      memberObject = message.guild.member(userObject);
-      username = userObject.tag;
-      userAvatar = userObject.avatarURL();
-      userCreated = userObject.createdAt.toDateString() + ", " + userObject.createdAt.toLocaleTimeString('en-US');
-      userID = userObject.id;
-      userJoinedServer = memberObject.joinedAt.toDateString() + ", " + memberObject.joinedAt.toLocaleTimeString('en-US');
-      userRoles = memberObject.roles.cache.map(roles => roles).join(", ");
-      sortedmembers = message.guild.members.cache.array().sort((a, b) => {
-        a.joinedTimestamp - b.joinedTimestamp
-      })
-
-      userJoinPosition = sortedmembers.indexOf(memberObject) + 1
-    } else {
-      userObject = message.author;
-      memberObject = message.guild.member(userObject);
-      username = userObject.tag;
-      userAvatar = userObject.avatarURL();
-      userCreated = userObject.createdAt.toDateString() + ", " + userObject.createdAt.toLocaleTimeString('en-US');
-      userID = userObject.id;
-      userJoinedServer = memberObject.joinedAt.toDateString() + ", " + memberObject.joinedAt.toLocaleTimeString('en-US');
-      userRoles = memberObject.roles.cache.map(roles => roles).join(", ");
-      sortedmembers = message.guild.members.cache.array().sort((a, b) => {
-        a.joinedTimestamp - b.joinedTimestamp
-      })
-
-      userJoinPosition = sortedmembers.indexOf(memberObject) + 1
-    }
+      const userObject = message.mentions.users.size
+        ? message.mentions.users.first()
+        : message.author;
+      const memberObject = message.guild.member(userObject);
+      const username = userObject.tag;
+      const userAvatar = userObject.avatarURL();
+      const userCreated = userObject.createdAt.toDateString() + ", " + userObject.createdAt.toLocaleTimeString('en-US');
+      const userID = userObject.id;
+      const userJoinedServer = memberObject.joinedAt.toDateString() + ", " + memberObject.joinedAt.toLocaleTimeString('en-US');
+      const userRoles = memberObject.roles.cache.map(roles => roles).join(", ");
+      const sortedmembers = message.guild.members.cache.array();
+      sortedmembers.sort((a, b) => a.joinedTimestamp - b.joinedTimestamp)
+      const userJoinPosition = sortedmembers.indexOf(memberObject) + 1
     const modEmbed = new Discord.MessageEmbed()
       .setColor('#32CD32')
       .setAuthor(username)
