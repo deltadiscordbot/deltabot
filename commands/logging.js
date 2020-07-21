930da848fa409a641880191d52f674c8f5f31b47
Jimmy <jimmyshadow0@gmail.com>
2020-07-21 13:04:39 -0500

    changed logging option to helper

--- commands/logging.js
+++ commands/logging.js
@@ -1,9 +1,9 @@
 module.exports = {
     name: 'logging',
-    description: 'Enables logging. (Owner only)',
-    needsowner: true,
+    description: 'Enables logging. (Helper only)',
+    needshelper: true,
     cooldown: 1,
-    category: "owner",
+    category: "mod",
     async execute(message, args) {
         message.client.logging = !message.client.logging;
         message.reply(`logging set to ${message.client.logging}`)
